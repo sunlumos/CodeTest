377595e1eaaacb559bf19d18c60f790a7ad83760
sunlumos <735287480@qq.com>
2023-06-11 17:56:25 +0800

    数据更新

--- seedtrain/SVC.py
+++ seedtrain/SVC.py
@@ -4,15 +4,15 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
-df = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\jiafengyouData\jiafengyou-train.csv", header=None)
+df = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\yongyou1540Data\yongyou1540-train.csv", header=None)
 train_targets = df.values[:,224]
 train_data = df.values[0:800,19:201]
 #print(train_targets)
-df2 = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\jiafengyouData\jiafengyou-test.csv", header=None)
+df2 = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\yongyou1540Data\yongyou1540-test.csv", header=None)
 test_targets = df2.values[:,224]
 test_data = df2.values[0:800,19:201]
 #print(df2.head(5))
-df3 = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\jiafengyouData\jiafengyou-val.csv", header=None)
+df3 = pd.read_csv("D:\S\start\code\CodeTest\seedtrain\yongyou1540Data\yongyou1540-val.csv", header=None)
 pre_targets = df3.values[:,224]
 pre_data = df3.values[0:800,19:201]
 
@@ -23,10 +23,11 @@ print("训练数据集长度为：{}".format(train_tensor_len))#"训练数据集
 print("验证数据集长度为：{}".format(test_tensor_len))
 print("测试数据集长度为：{}".format(pre_tensor_len))
 
-# model=svm.SVC(C=2,kernel='linear',gamma=10,)
-# svm.SVC(C=1,kernel='poly',degree=3,)
-# svm.SVC(C=1000,kernel='rbf',gamma=1,)
-model = svm.SVC(C=1000,kernel='rbf',gamma=0.1,)
+# svm.SVC(C=2,kernel='linear',gamma=10,) 训练集： 0.76875 验证集： 0.635 测试集： 0.635
+# svm.SVC(C=1,kernel='poly',degree=3,) 训练集： 0.94625 验证集： 0.86 测试集： 0.85
+# svm.SVC(C=1000,kernel='rbf',gamma=1,) 训练集： 0.9375 验证集： 0.83 测试集： 0.845
+# svm.SVC(C=1000,kernel='rbf',gamma=0.1,) 0.89625 0.79 0.76
+model = svm.SVC(C=2,kernel='linear',gamma=10,)
 model.fit(train_data,train_targets)
 
 train_score = model.score(train_data,train_targets)
